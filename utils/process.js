56650e29cfc19a6cc590a1e0ae2866950246b0bf
carson-young18 <Carson_Young@insideranken.org>
2025-05-26 01:38:40 -0500

    Update process.js

--- utils/process.js
+++ utils/process.js
@@ -3,7 +3,7 @@ import db from './db.js';
 export default async function processData(playerData) {
   for(const data of playerData){
     for(const activity of data.profile.activities){
-      console.log(activity.text.replace("I ", data.player));
+      //YOU WERE HERE!
     }
   }
 
